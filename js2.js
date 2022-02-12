4ecaaf5862b6ad58e9d20592a69fd2ea62fc4c0e
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-02-12 18:52:39 +0600

    finished task2 cars

--- js2.js
+++ js2.js
@@ -7,12 +7,31 @@ class Cars {
   this.color = color;
   this.engine = engine;
 }
-};
 
 showInfo() {
-  console.log(
+  return(
     `Model: ${this.model}, Year: ${this.year}, Color: ${this.color}, Engine type : ${this.engine}`
   )};
+};
 
 
 
+class Electrocars extends Cars {
+    constructor(model, year, color, engine, batery) {
+        super(model, year, color, engine);
+        this.batery = batery;
+    }
+
+   promote() {
+       return(`We have best electocars ever! Follow us and enjoy!`)
+   };
+    
+};
+
+
+const bmw = new Cars("bmw", "2009", "red", "diesel");
+const tesla = new Electrocars("Tesla",2019, "white", "electro", 5);
+
+ console.log(bmw.showInfo());
+console.log(tesla.showInfo());
+console.log(tesla.promote());
